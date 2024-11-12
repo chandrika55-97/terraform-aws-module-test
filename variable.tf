9404cb2a0bee5e87d7d5feb481e5e0e379d1e025
chandrika <chandrikaaws55@gmail.com>
2024-11-12 17:21:23 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,7 +4,9 @@ variable "project_name"{
 variable "environment"{
     default = "dev"
 }
-
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
 variable "common_tags"{
     default = {
         Project = "myexpense-project"
