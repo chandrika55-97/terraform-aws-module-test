9aa375283cf254895985a37f967fb49b8f45b276
chandrika <chandrikaaws55@gmail.com>
2024-11-12 11:28:49 +0530

    added souce value is github path

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,9 @@
 output "vpc_id"{   #this is for understand of vpc id
     value = module.vpc.vpc_id
 }
+output "public_subnet_ids"{
+    value = module.vpc.public_subnet_ids
+}
 # output "az_info"{
 #    value = module.vpc.az_info
 # }
